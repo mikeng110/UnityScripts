b6c3d07c0d9b504c6271dae4a0dfee2773cae402
mikeng110 <mikael.englund110@gmail.com>
2020-07-19 20:46:36 +0200

    Added camera follows
    This script make the camera follow the player in a smooth manner.

---
+++ 2dPlatformer/CameraFollows.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollows : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5;
+
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        //Move camera to player in smooth transition.
+        Vector3 targetCamPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPosition, smoothing * Time.deltaTime);
+    }
+}
