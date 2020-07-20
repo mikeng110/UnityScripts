15d95f4094aff65388f788af252045b64333caae
mikeng110 <mikael.englund110@gmail.com>
2020-07-20 13:25:07 +0200

    added coincontroller and playerinventory

---
+++ 2dPlatformer/CoinController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            GetPlayerInventory(other.gameObject).IncrementCoins();
+            Destroy(transform.root.gameObject);
+        }
+    }
+
+    private PlayerInventory GetPlayerInventory(GameObject obj)
+    {
+        PlayerInventory result = obj.GetComponent<PlayerInventory>();
+
+        if (result == null)
+        {
+            throw new System.NullReferenceException("PlayerInventory Script, Missing From Player Prefab");
+        }
+        return result;
+    }
+}
---
+++ 2dPlatformer/PlayerInventory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    private int coins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coins = 0;
+    }
+
+    public void IncrementCoins()
+    {
+        coins++;
+        Debug.Log($"Nr of coins {coins}");
+    }
+}
